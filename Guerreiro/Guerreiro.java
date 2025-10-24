10f47f51412510927fcc4b67a60f5b5574770ec0
ArturCDA <artur.crispim@academico.ifpb.edu.br>
2025-10-23 21:25:25 -0300

    Add files via upload

---
+++ Guerreiro/Guerreiro.java
@@ -0,0 +1,97 @@
+package Guerreiro;
+import java.util.Random;
+
+public class Guerreiro {
+    private int codigo;
+    private String nome;
+    private int energia;
+
+    public Guerreiro (int codigo, String nome){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.energia = 5;
+    }
+
+    public int getenergia (){
+        return energia;
+    }
+
+    public void incremento (){
+        if (energia >= 0 && energia < 5){
+            energia += 1;
+        } else {
+        
+        }
+    }
+
+    public void decremento (){
+        if (energia <= 5 && energia > 0){
+            energia -= 1;
+        } 
+        else if (energia == 0){
+        } else {
+            
+        }
+    }
+
+    public void alimentar (){
+        incremento();
+    }
+
+    public int atacar (){
+        Random gerador = new Random();
+        return gerador.nextInt(2);
+    }
+
+    public void lutarL (Guerreiro oponente){
+        while (energia > 0 && oponente.getenergia() > 0){
+            int ag = atacar();
+            int ao = oponente.atacar();
+            if (ag == 1){
+                oponente.decremento();
+                if (oponente.getenergia() == 0){
+                break;
+                }
+            }
+            if (ao == 1) {
+                decremento();
+                if (energia == 0){
+                break;
+                }
+            }
+        }
+    }
+
+    public void lutarR (Guerreiro oponente){
+        if (energia <= 0 || oponente.getenergia() <= 0) {
+            return;
+        }
+
+        int ag = atacar();
+        int ao = oponente.atacar();
+
+        if (ag == 1) {
+            oponente.decremento();
+            if (oponente.getenergia() == 0) {
+                return;
+            }
+        }
+
+        if (ao == 1) {
+            decremento();
+            if (energia == 0) {
+                return;
+            }
+        }
+
+        lutarR(oponente);
+    }
+
+    public String toString(){
+        return "Guerreiro - " + "\n" +
+        "Código = " + codigo + "\n" +
+        "Nome = " + nome + "\n" +
+        "Energia = " + energia + "\n" +
+        ".";
+    }
+}
---
+++ Guerreiro/Principal_Guerreiro.java
@@ -0,0 +1,28 @@
+package Guerreiro;
+
+public class Principal_Guerreiro {
+    public static void main(String[] args) {
+        Guerreiro guerreiro1 = new Guerreiro(2, "Gohan");
+        Guerreiro guerreiro2 = new Guerreiro(1, "Cell");
+
+        guerreiro1.lutarL(guerreiro2);
+        System.out.println(guerreiro1);
+        System.out.println(guerreiro2);
+
+        guerreiro1.alimentar();
+        guerreiro2.alimentar();
+        System.out.println(guerreiro1);
+        System.out.println(guerreiro2);
+
+        guerreiro1.lutarR(guerreiro2);
+        System.out.println(guerreiro1);
+        System.out.println(guerreiro2);
+
+        guerreiro1.alimentar();
+        guerreiro2.alimentar();
+        System.out.println(guerreiro1);
+        System.out.println(guerreiro2);
+    }
+    
+    
+}
